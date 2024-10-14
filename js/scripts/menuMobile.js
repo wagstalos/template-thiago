d1f2d504c0dcd9dfc6c88a60dc68850a470c36dd
Wagner Paulo <wagstalos@hotmail.com>
2024-10-14 10:28:59 -0300

    feat: menu mobile

--- js/scripts/menuMobile.js
+++ js/scripts/menuMobile.js
@@ -1,3 +1,29 @@
+// function menuMobile() {
+//   var dotsButton = document.querySelector(".dots");
+//   var menuMobile = document.querySelector(".menu-mobile");
+//   var closeIcon = document.querySelector(".icon-tabler-x");
+//   var links = document.querySelectorAll(".menu-mobile__links a");
+
+//   dotsButton.addEventListener("click", function () {
+//     menuMobile.style.display = "block";
+//     closeIcon.style.display = "block";
+//   });
+
+//   closeIcon.addEventListener("click", function () {
+//     menuMobile.style.display = "none";
+//     closeIcon.style.display = "none";
+//   });
+
+//   links.forEach(function (link) {
+//     link.addEventListener("click", function () {
+//       menuMobile.style.display = "none";
+//       closeIcon.style.display = "none";
+//     });
+//   });
+// }
+
+// menuMobile();
+
 function menuMobile() {
   var dotsButton = document.querySelector(".dots");
   var menuMobile = document.querySelector(".menu-mobile");
@@ -5,19 +31,19 @@ function menuMobile() {
   var links = document.querySelectorAll(".menu-mobile__links a");
 
   dotsButton.addEventListener("click", function () {
-    menuMobile.style.display = "block";
-    closeIcon.style.display = "block";
+    menuMobile.classList.toggle("active"); // Alterna a classe 'active'
+    closeIcon.classList.toggle("active"); // Alterna a classe 'active' para o ícone de fechar
   });
 
   closeIcon.addEventListener("click", function () {
-    menuMobile.style.display = "none";
-    closeIcon.style.display = "none";
+    menuMobile.classList.remove("active"); // Remove a classe 'active' ao clicar no ícone de fechar
+    closeIcon.classList.remove("active");
   });
 
   links.forEach(function (link) {
     link.addEventListener("click", function () {
-      menuMobile.style.display = "none";
-      closeIcon.style.display = "none";
+      menuMobile.classList.remove("active"); // Remove a classe 'active' ao clicar em qualquer link
+      closeIcon.classList.remove("active");
     });
   });
 }
